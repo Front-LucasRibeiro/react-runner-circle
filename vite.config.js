4ae815043adb580f0734859bcc3fdd4740e933af
Lucas Maranho <lksribeiro2014@gmail.com>
2025-10-19 21:07:20 -0300

    fix: adiciona configuração do GraphQL no Vite para resolver erro 'Delete is not defined'

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Fix para Apollo Client
+      'graphql': 'graphql/index.js'
+    }
+  },
+  optimizeDeps: {
+    include: ['graphql']
+  }
 })
